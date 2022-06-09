00ea00c6b7c5bcee439876ea17e4c1ccc9219d51
Andrea Lauri <2410325+alauri@users.noreply.github.com>
2022-06-09 18:03:22 +0200

    Command 'insert' to store new records into the db

--- examples/python/oha/cmd/insert.py
+++ examples/python/oha/cmd/insert.py
@@ -1,11 +1,70 @@
 #!/usr/bin/python3
 
 
+"""Command ``insert`` is used store new records within the db.
+
+It can repeat the same operation in loop or a defined numbers of times. It can
+be possible to define a delay between one operation and the next one and also
+after how many operations commit the changes.
+"""
+
+
+import time
 import click
 
 
 @click.command()
-def insert() -> None:
-    """Insert new records into the table.
-    """
-    click.echo("insert called")
+@click.option('--loop/--no-loop',
+              type=bool,
+              default=False,
+              help="repeat the same operation forever")
+@click.option('--iters',
+              type=int,
+              default=1,
+              help='repeat the same operation a given number of times')
+@click.option('--delay',
+              type=float,
+              default=0.25,
+              help='time to wait before the next iteration')
+@click.option('--commit-every',
+              type=int,
+              default=1,
+              help='after how many operations perform a commit')
+@click.pass_obj
+def insert(obj,
+           loop: bool,
+           iters: int,
+           delay: float,
+           commit_every: int) -> None:
+    """Insert new records within the table"""
+
+    # Define query parameters
+    table = obj.conf["database"]["table"]
+    conds = {"id": 0}
+
+    iters = 0 if loop else iters
+    step = 1
+    try:
+        while loop or step <= iters:
+            # Prepare query with updated conditions
+            values = [str(val + step) for val in conds.values()]
+            query = (f"INSERT INTO {table}({', '.join(conds.keys())}) "
+                     f"VALUES({', '.join(values)})")
+
+            # TODO: replace with call to cx_Oracle driver
+            click.echo(f"[{step}/{iters}] - {query}")
+
+            # Commit changes
+            if step % commit_every == 0:
+                # TODO: insert a database commit operation here
+                click.echo(f"[{step}/{iters}] - COMMIT")
+
+            step += 1
+            time.sleep(delay)
+    except KeyboardInterrupt as _:
+        click.echo("Error - Interrupted by the user")
+
+    # Check the last commit
+    if iters % commit_every != 0:
+        # TODO: insert a database commit operation here
+        click.echo(f"[{iters}/{iters}] - COMMIT")
