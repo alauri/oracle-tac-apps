2fa9692b2128df6bf0d8257bde7cec7232963093
Andrea Lauri <andrea.lauri86@gmail.com>
2022-06-19 20:42:48 +0200

    Command 'insert' for Golang

--- examples/golang/oracle-ha/cmd/insert.go
+++ examples/golang/oracle-ha/cmd/insert.go
@@ -6,16 +6,66 @@ package cmd
 
 import (
 	"fmt"
+    "time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // insertCmd represents the insert command
 var insertCmd = &cobra.Command{
 	Use:   "insert",
-	Short: "Insert new records into the table.",
-	Long: `Insert new records into the table.`,
+	Short: "Insert new records into the table",
+	Long: `Insert new records into the table`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Fprintln(cmd.OutOrStdout(), "insert called")
+        loop, _ := cmd.Flags().GetBool("loop")
+        iters, _ := cmd.Flags().GetInt("iters")
+        delay, _ := cmd.Flags().GetFloat64("delay")
+        commit_every, _ := cmd.Flags().GetInt("commit-every")
+
+        // Define query parameters
+        table := viper.GetViper().GetString("database.table")
+        conditions := conds{0}
+
+        if loop { iters = 0 }
+        step := 1
+        for {
+            // Exit condition
+            if !loop && step > iters {
+                break
+            }
+
+            // Prepare query with updated conditions
+            pairs := conds{conditions.id + step}
+            query := fmt.Sprintf("INSERT INTO %s(id) VALUES(%d)", table, pairs.id)
+
+            // TODO: replace with call to cx_Oracle driver
+            fmt.Fprintln(cmd.OutOrStdout(),
+                         fmt.Sprintf("[%d/%d] - %s", step, iters, query))
+
+            // Commit changes
+            if step % commit_every == 0 {
+                // TODO: insert a database commit operation here
+                fmt.Fprintln(cmd.OutOrStdout(),
+                             fmt.Sprintf("[%d/%d] - COMMIT", step, iters))
+            }
+
+            step += 1
+            time.Sleep(time.Duration(delay) * time.Millisecond)
+        }
+
+        // Chech the last commit
+        if iters % commit_every != 0 {
+            // TODO: insert a database commit operation here
+            fmt.Fprintln(cmd.OutOrStdout(),
+                         fmt.Sprintf("[%d/%d] - COMMIT", iters, iters))
+        }
 	},
+}
+
+func init() {
+    insertCmd.Flags().Bool("loop", false, "repeat the same operation forever")
+    insertCmd.Flags().Int("iters", 1, "repeat the same operation a given number of times")
+    insertCmd.Flags().Float64("delay", 0.25, "time to wait before the next iteration")
+    insertCmd.Flags().Int("commit-every", 1, "after how many operations perform a commit")
 }
--- examples/golang/oracle-ha/cmd/insert_test.go
+++ examples/golang/oracle-ha/cmd/insert_test.go
@@ -1,21 +1,64 @@
+/*
+Copyright © 2022 Andrea Lauri <andrea.lauri86@gmail.com>
+
+Tests for the package ``insert.go``
+*/
 package cmd
 
 import (
     "testing"
     "bytes"
+    "runtime"
+    "path"
+    "strings"
 
     "github.com/stretchr/testify/assert"
 )
 
 
-func Test_InsertCommand(t *testing.T) {
+func Test_Insert_No_Args(t *testing.T) {
+    // Invoke the command ``insert`` with no options.
+
+    _, filename, _, _ := runtime.Caller(0)
+    static := path.Join(path.Dir(filename), "../static")
+
     actual := new(bytes.Buffer)
 
     rootCmd.SetOut(actual)
     rootCmd.SetErr(actual)
-    rootCmd.SetArgs([]string{"insert"})
+    rootCmd.SetArgs([]string{"-w", static, "insert"})
     rootCmd.Execute()
 
-    expected := "insert called\n"
-    assert.Equal(t, expected, actual.String())
+    expected := []string{"[1/1] - INSERT INTO test(id) VALUES(1)",
+                         "[1/1] - COMMIT"}
+
+    splitFn := func(c rune) bool { return c == '\n' }
+    assert.Equal(t, expected, strings.FieldsFunc(actual.String(), splitFn))
+}
+
+
+func Test_Insert_With_Args(t *testing.T) {
+    // Invoke the command ``insert`` with the options to iterate the same
+    // operation multiple times.
+
+    _, filename, _, _ := runtime.Caller(0)
+    static := path.Join(path.Dir(filename), "../static")
+
+    actual := new(bytes.Buffer)
+
+    rootCmd.SetOut(actual)
+    rootCmd.SetErr(actual)
+    rootCmd.SetArgs([]string{"-w", static, "insert", "--iters", "5", "--delay", "0.05", "--commit-every", "2"})
+    rootCmd.Execute()
+
+    expected := []string{"[1/5] - INSERT INTO test(id) VALUES(1)",
+                         "[2/5] - INSERT INTO test(id) VALUES(2)",
+                         "[2/5] - COMMIT",
+                         "[3/5] - INSERT INTO test(id) VALUES(3)",
+                         "[4/5] - INSERT INTO test(id) VALUES(4)",
+                         "[4/5] - COMMIT",
+                         "[5/5] - INSERT INTO test(id) VALUES(5)",
+                         "[5/5] - COMMIT"}
+    splitFn := func(c rune) bool { return c == '\n' }
+    assert.Equal(t, expected, strings.FieldsFunc(actual.String(), splitFn))
 }
