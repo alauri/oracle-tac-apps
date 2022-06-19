88335a1cafc427c60ca73be682cce416b110e2c6
Andrea Lauri <2410325+alauri@users.noreply.github.com>
2022-06-19 20:48:29 +0200

    Command 'reset' for Golang

--- examples/golang/oracle-ha/cmd/reset_test.go
+++ examples/golang/oracle-ha/cmd/reset_test.go
@@ -1,21 +1,33 @@
+/*
+Copyright © 2022 Andrea Lauri <andrea.lauri86@gmail.com>
+
+Tests for the package ``reset.go``
+*/
 package cmd
 
 import (
     "testing"
     "bytes"
+    "runtime"
+    "path"
 
     "github.com/stretchr/testify/assert"
 )
 
 
-func Test_ResetCommand(t *testing.T) {
+func Test_Reset_No_Args(t *testing.T) {
+    // Invoke the command ``reset`` with no options.
+
+    _, filename, _, _ := runtime.Caller(0)
+    static := path.Join(path.Dir(filename), "../static")
+
     actual := new(bytes.Buffer)
 
     rootCmd.SetOut(actual)
     rootCmd.SetErr(actual)
-    rootCmd.SetArgs([]string{"reset"})
+    rootCmd.SetArgs([]string{"-w", static, "reset"})
     rootCmd.Execute()
 
-    expected := "reset called\n"
+    expected := "[+] - Database has been reset\n"
     assert.Equal(t, expected, actual.String())
 }
