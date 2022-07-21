ab538fffc943f80b7eb847b55574a4fc1cbb8fce
Andrea Lauri <andrea.lauri86@gmail.com>
2022-07-21 23:45:31 +0200

    Read dump and injest data

--- apps/python/tests/test_injest.py
+++ apps/python/tests/test_injest.py
@@ -13,35 +13,42 @@ def test_no_args(runner, static) -> None:
     Returns:
         Nothing
     """
-    result = runner.invoke(cli, ["-w", static, 'injest'])
+    result = runner.invoke(cli, ["-w", static,
+                                 "injest"])
 
     assert result.exit_code == 0
 
     output = [l for l in result.output.split("\n") if l]
-    assert output == ['[1/1] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(1, \'pippo\')',
-                      '[1/1] - COMMIT']
+    assert output == [
+        "[1/1] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,3,Right=False|Left=True)",
+        "[1/1] - COMMIT"]
 
 
 def test_args(runner, static) -> None:
-    """Invoke the command ``delete`` with the options ``iters``.
+    """Invoke the command ``injest`` with the options ``iters``.
 
     Returns:
         Nothing
     """
-    result = runner.invoke(cli, ["-w", static, 'injest',
+    result = runner.invoke(cli, ["-w", static,
+                                 "injest",
                                  "--iters", 5,
                                  "--delay", 0.05,
-                                 "--commit-every", 2
-                                 ])
+                                 "--commit-every", 5])
 
     assert result.exit_code == 0
 
     output = [l for l in result.output.split("\n") if l]
-    assert output == ['[1/5] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(1, \'pippo\')',
-                      '[2/5] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(2, \'pippo\')',
-                      '[2/5] - COMMIT',
-                      '[3/5] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(3, \'pippo\')',
-                      '[4/5] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(4, \'pippo\')',
-                      '[4/5] - COMMIT',
-                      '[5/5] - INSERT INTO test(DEPARTMENT_ID, DEPARTMENT_NAME) VALUES(5, \'pippo\')',
-                      '[5/5] - COMMIT']
+    assert output == [
+        "[1/5] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,3,Right=False|Left=True)",
+        "[2/5] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,2,Right=False|Left=True)",
+        "[3/5] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,2,Right=False|Left=True)",
+        "[4/5] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,1,Right=False|Left=True)",
+        "[5/5] - INSERT INTO raw_table(timestamp,sensorid,data) " \
+            "VALUES(1658351188,1,Right=False|Left=True)",
+        "[5/5] - COMMIT"]
