99a63d70bb02d3d81017aee1dd0a903a2868bc2c
Andrea Lauri <2410325+alauri@users.noreply.github.com>
2022-07-22 12:28:35 +0200

    Clean up raw data and store them into the db

---
+++ apps/python/tests/factory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+"""Mock classes to simulate cx_Oracle's objects and their behaviours."""
+
+
+class MockResponse:
+
+    def fetchone(self, *args, **kwargs):
+        pass
+
+
+class MockCursor:
+
+    def execute(self, *args, **kwargs):
+        return MockResponse()
+
+
+class MockOracle:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def cursor(self, *args, **kwargs):
+        return MockCursor()
+
+    def commit(self, *args, **kwargs):
+        return MockCursor()
