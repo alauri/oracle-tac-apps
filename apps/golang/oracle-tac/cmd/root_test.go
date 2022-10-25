2a7b93ce1af5d9d41ec7c2817092c4908b305095
Andrea Lauri <2410325+alauri@users.noreply.github.com>
2022-10-25 22:33:39 +0200

    Refactor command rootCmd

--- apps/golang/oracle-tac/cmd/root_test.go
+++ apps/golang/oracle-tac/cmd/root_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Root(t *testing.T) {
-	// Invoke the main command.
+func Test_Root_usage(t *testing.T) {
+	// Invoke the CLI with no commands, expecting an 'Usage ...' message
 
 	_, filename, _, _ := runtime.Caller(0)
 	static := path.Join(path.Dir(filename), "../static")
@@ -23,9 +23,53 @@ func Test_Root(t *testing.T) {
 	actual := new(bytes.Buffer)
 	rootCmd.SetOut(actual)
 	rootCmd.SetErr(actual)
-	rootCmd.SetArgs([]string{"-w", static, "-d", "5"})
+	rootCmd.SetArgs([]string{"-w", static})
 	rootCmd.Execute()
 
-	expected := "Use \"oracle-tac-go [command] --help"
-	assert.Contains(t, actual.String(), expected)
+	assert.Contains(t, actual.String(), "oracle-tac-go [flags]")
+}
+
+func Test_Root_config(t *testing.T) {
+	// Invoke the CLI by asking information about the current configuration.
+
+	_, filename, _, _ := runtime.Caller(0)
+	static := path.Join(path.Dir(filename), "../static")
+
+	actual := new(bytes.Buffer)
+	rootCmd.SetOut(actual)
+	rootCmd.SetErr(actual)
+	rootCmd.SetArgs([]string{"-w", static, "--config"})
+	rootCmd.Execute()
+
+	assert.Contains(t, actual.String(), "{")
+	assert.NotContains(t, actual.String(), "Usage:")
+}
+
+func Test_Root_ping(t *testing.T) {
+	// Invoke the CLI by asking to ping the database.
+
+	_, filename, _, _ := runtime.Caller(0)
+	static := path.Join(path.Dir(filename), "../static")
+
+	actual := new(bytes.Buffer)
+	rootCmd.SetOut(actual)
+	rootCmd.SetErr(actual)
+	rootCmd.SetArgs([]string{"-w", static, "-d", "localhost", "--ping"})
+	rootCmd.Execute()
+
+	assert.Contains(t, actual.String(), "[+] - Database reachable")
+}
+
+func Test_Root_error(t *testing.T) {
+	// Invoke the CLI with a wrong dsn value.
+
+	_, filename, _, _ := runtime.Caller(0)
+	static := path.Join(path.Dir(filename), "../static")
+
+	actual := new(bytes.Buffer)
+	rootCmd.SetOut(actual)
+	rootCmd.SetErr(actual)
+	rootCmd.SetArgs([]string{"-w", static, "-d", "wrong", "ingest"})
+
+	assert.PanicsWithValue(t, "Invalid value for '-d/--dsn'", func() { rootCmd.Execute() })
 }
